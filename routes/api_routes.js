b42fb73cbb969f6b02222764917d08822c21bc6f
Portmeister <portmeister@gmail.com>
2021-02-24 12:57:33 -0600

    rearranging code

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
     .then(dbWorkout => {
@@ -21,15 +32,6 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+
 
 module.exports = router;
