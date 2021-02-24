f55e45388092e6f0b78f00394a6424d5e3f586fe
Portmeister <portmeister@gmail.com>
2021-02-24 13:49:18 -0600

    update api routes to create, read and update

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,36 +2,53 @@ const router = require("express").Router();
 const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
-  db.Workout.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
+  db.Workout.find({}).then(dbWorkout => {
+      dbWorkout.forEach(workout => {
+          var total = 0;
+          workout.exercises.forEach(e => {
+              total += e.duration;
+          });
+          workout.totalDuration = total;
+      });
       res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+  }).catch(err => {
+      res.json(err);
+  });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+          $inc: { totalDuration: req.body.duration },
+          $push: { exercises: req.body }
+      },
+      { new: true }).then(dbWorkout => {
+          res.json(dbWorkout);
+      }).catch(err => {
+          res.json(err);
+      });
 });
 
 router.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
-    .then(dbWorkout => {
+    .then((dbWorkout => {
       res.json(dbWorkout);
-    })
+    }))
     .catch(err => {
-      res.status(400).json(err);
+      res.json(err);
     });
 });
 
-router.post("/api/workouts", ({ body }, res) => {
-  db.Workout.insertMany(body)
-    .then(dbWorkout => {
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({}).then(dbWorkout => {
+      console.log("ALL WORKOUTS");
+      console.log(dbWorkout);
+
       res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+  }).catch(err => {
+      res.json(err);
+  });
 });
-
-
 
 module.exports = router;
